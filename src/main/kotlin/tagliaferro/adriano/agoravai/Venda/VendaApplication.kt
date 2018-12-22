9cb059fe293161d5693e6c0493782d890c7ca43c
dritoferro <adrianotagliaferro@hotmail.com>
2018-12-22 11:14:10 -0200

    Created project structure

---
+++ src/main/kotlin/tagliaferro/adriano/agoravai/Venda/VendaApplication.kt
@@ -0,0 +1,12 @@
+package tagliaferro.adriano.agoravai.Venda
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VendaApplication
+
+fun main(args: Array<String>) {
+	runApplication<VendaApplication>(*args)
+}
+
---
+++ src/test/kotlin/tagliaferro/adriano/agoravai/Venda/VendaApplicationTests.kt
@@ -0,0 +1,17 @@
+package tagliaferro.adriano.agoravai.Venda
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class VendaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
