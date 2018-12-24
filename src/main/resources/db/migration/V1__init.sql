b2e6e22db40d09315db2b9b4bab8c7dc12722d68
dritoferro <adrianotagliaferro@hotmail.com>
2018-12-23 22:05:53 -0200

    Updated table's schema

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -10,14 +10,81 @@ CREATE TABLE ENDERECO(
     PRIMARY KEY(_id)
 );
 
-CREATE TABLE USUARIO(
+CREATE TABLE TELEFONE(
     _id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
+    telefone_celular VARCHAR(15) NOT NULL,
+    telefone_residencial VARCHAR(15),
+    telefone_comercial VARCHAR(15),
+    telefone_recados VARCHAR(15),
+    PRIMARY KEY(_id)
+);
+
+CREATE TABLE CLIENTE(
+    _id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
     data_nascimento DATE NOT NULL,
     endereco_id INT NOT NULL,
-    telefone_celular VARCHAR(15) NOT NULL,
-    telefone_residencial VARCHAR(14),
+    telefone_id INT NOT NULL,
     email VARCHAR(100) NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(endereco_id) REFERENCES ENDERECO(_id)
+    FOREIGN KEY(endereco_id) REFERENCES ENDERECO(_id),
+    FOREIGN KEY(telefone_id) REFERENCES TELEFONE(_id)
+);
+
+CREATE TABLE FORNECEDOR(
+    _id INT NOT NULL AUTO_INCREMENT,
+    razao_social VARCHAR(400) NOT NULL,
+    nome_fantasia VARCHAR(400) NOT NULL,
+    cnpj VARCHAR(18) NOT NULL,
+    endereco_id INT NOT NULL,
+    telefone_id INT NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    PRIMARY KEY(_id),
+    FOREIGN KEY(endereco_id) REFERENCES ENDERECO(_id),
+    FOREIGN KEY(telefone_id) REFERENCES TELEFONE(_id)
+);
+
+CREATE TABLE PRODUTO(
+    _id INT NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(500) NOT NULL,
+    fornecedor_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    preco DOUBLE NOT NULL,
+    PRIMARY KEY(_id),
+    FOREIGN KEY(fornecedor_id) REFERENCES FORNECEDOR(_id)
+);
+
+CREATE TABLE PEDIDO(
+    _id INT NOT NULL AUTO_INCREMENT,
+    item_pedido_id VARCHAR(500) NOT NULL,
+    total DOUBLE NOT NULL,
+    data_pedido DATE NOT NULL,
+    PRIMARY KEY(_id)
+);
+
+CREATE TABLE ITEM_PEDIDO(
+    _id INT NOT NULL AUTO_INCREMENT,
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    subtotal DOUBLE NOT NULL,
+    PRIMARY KEY(_id),
+    FOREIGN KEY(produto_id) REFERENCES PRODUTO(_id)
+);
+
+CREATE TABLE VENDA_FINAL(
+    _id INT NOT NULL AUTO_INCREMENT,
+    pedido_id INT NOT NULL,
+    cliente_id INT,
+    total DOUBLE NOT NULL,
+    desconto DOUBLE NOT NULL,
+    valor_pago DOUBLE NOT NULL,
+    valor_troco DOUBLE NOT NULL,
+    data_venda DATE NOT NULL,
+    forma_pagamento VARCHAR(100) NOT NULL,
+    quantidade_parcela INT NOT NULL,
+    valor_parcela DOUBLE NOT NULL,
+    PRIMARY KEY(_id),
+    FOREIGN KEY(pedido_id) REFERENCES PEDIDO(_id),
+    FOREIGN KEY(cliente_id) REFERENCES CLIENTE(_id)
 );
